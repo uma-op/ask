a1fee0f091814a226532ac56e68debce5494d088
Arseniy Bannikov <bannikovtop@gmail.com>
2023-04-03 16:20:31 +0300

    Форма задачи вопроса

--- ask/app/urls.py
+++ ask/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('ask/', views.ask, name='ask'),
+    path('settings/', views.settings, name='settings')
 ]
 
--- ask/app/views.py
+++ ask/app/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
-
 def index(request):
     template = loader.get_template('app/new_questions.html')
+    context = dict()
+    return HttpResponse(template.render(context, request))
+
+def ask(request):
+    template = loader.get_template('app/ask.html')
+    context = dict()
+    return HttpResponse(template.render(context, request))
+
+def settings(request):
+    template = loader.get_template('app/settings.html')
     context = dict()
     return HttpResponse(template.render(context, request))
 
