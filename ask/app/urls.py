382717ef8933bc2c659bceba783223bb01f0c404
Arseniy Bannikov <bannikovtop@gmail.com>
2023-04-08 12:28:51 +0300

    Страница вопросов по тегу

--- ask/app/urls.py
+++ ask/app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ask/', views.ask, name='ask'),
+    path('tag/<str:tag>', views.tag, name="tag"),
     path('settings/', views.settings, name='settings'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup')
--- ask/app/views.py
+++ ask/app/views.py
@@ -73,7 +73,80 @@ def index(request):
 
 def ask(request):
     template = loader.get_template('app/ask.html')
-    context = dict()
+    context = {
+        "popular_tags": [
+            "tag1",
+            "tag2",
+            "tag3",
+            "tag4"
+        ],
+        "best_members": [
+            "best member 1",
+            "best member 2",
+            "best member 3"
+        ]
+    }
+    return HttpResponse(template.render(context, request))
+
+def tag(request, tag):
+    template = loader.get_template('app/tag_questions.html')
+    context = {
+        "tag": tag,
+        "questions": [ 
+            Question(
+                "theme1",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                1,
+                ["tag1"],
+                "img/ava.png",
+                1,
+            ),
+            Question(
+                "theme2",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                2,
+                ["tag1", "tag2"],
+                "img/ava.png",
+                2,
+            ),
+            Question(
+                "theme3",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                3,
+                ["tag3"],
+                "img/ava.png",
+                4,
+            ),
+            Question(
+                "theme4",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                4,
+                [],
+                "img/ava.png",
+                8,
+            ),
+            Question(
+                "theme5",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                5,
+                ["tag_hueg"],
+                "img/ava.png",
+                16,
+            )
+        ],
+        "popular_tags": [
+            "tag1",
+            "tag2",
+            "tag3",
+            "tag4"
+        ],
+        "best_members": [
+            "best member 1",
+            "best member 2",
+            "best member 3"
+        ]
+    }
+
     return HttpResponse(template.render(context, request))
 
 def settings(request):
