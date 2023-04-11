c1b8f85b462831bae0c81a51ac78af5d7ad7b04d
Arseniy Bannikov <bannikovtop@gmail.com>
2023-04-11 19:01:30 +0300

    Созданы основные модели

--- ask/app/models.py
+++ ask/app/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    # id =
+    user = models.BigIntegerField()  # OneToOne to django user
+    login = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=255)
+    popularity = models.BigIntegerField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    popularity = models.BigIntegerField()
+
+class Question(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    theme = models.CharField(max_length=255)
+    preview_text = models.CharField(max_length=2048)
+    text = models.TextField()
+    marks = models.BigIntegerField()
+
+    tags = models.ManyToManyField(Tag)
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
+    text = models.TextField()
+    is_correct = models.BooleanField()
+    marks = models.BigIntegerField()
+
+class AnswerMark(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    answer = models.ForeignKey(
+        Answer,
+        on_delete=models.CASCADE
+    )
+
+class QuestionMark(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE
+    )
+
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE
+    )
+
--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "ask",
+        'USER': "uma_op",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
